b1485e6dc92904b36fadcfc5ca628fc3d5346c32
Deborah KOLAWOLE <deborah@anyapp.ltd>
2025-02-02 20:20:09 +0100

    created simple files for a cleaner code

--- lib/home.dart
+++ lib/home.dart
@@ -1,38 +1,18 @@
+import 'package:dok_app/widgets/app_links.dart';
 import 'package:dok_app/widgets/button/custom_button.dart';
+import 'package:dok_app/widgets/launchUrl.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
-  final String githubRepo = "https://github.com/dehbbyk/dok_app";
-  final String hngHirePage = "https://hng.tech/hire";
-  final String telex = "https://telex.ai";
-  final String delve = "https://delve.hng.tech";
-
-  void _launchURL(BuildContext context, String url) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text("Open Link"),
-        content: Text("Please copy the link and open it in your browser: \n$url"),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: Text("OK"),
-          ),
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text(
+          title: const Text(
               'DOK App',
             style: TextStyle(
-              fontSize: 30,
               fontWeight:  FontWeight.bold
             ),
           )
@@ -44,25 +24,33 @@ class Home extends StatelessWidget {
             CustomButton(
               text: 'View GitHub Repository',
               color: Colors.blue,
-              onPressed: () => _launchURL(context, githubRepo),
+              onPressed: () {
+                LaunchUrl.urlLaunch(context, AppLinks.githubRepo);
+              },
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
             CustomButton(
-              onPressed: () => _launchURL(context, hngHirePage),
               text: 'HNG Hire Pages',
               color: Colors.green,
+              onPressed: (){
+                LaunchUrl.urlLaunch(context, AppLinks.hngHirePage);
+              }
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
             CustomButton(
-              onPressed: () => _launchURL(context, telex),
               text: 'Telex',
               color: Colors.orange,
+                onPressed: () {
+                  LaunchUrl.urlLaunch(context, AppLinks.telex);
+                }
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
             CustomButton(
-              onPressed: () => _launchURL(context, delve),
               text: 'Delve',
               color: Colors.purple,
+                onPressed: (){
+                  LaunchUrl.urlLaunch(context, AppLinks.delve);
+              }
             ),
           ],
         ),
---
+++ lib/widgets/app_links.dart
@@ -0,0 +1,6 @@
+class AppLinks {
+  static const String githubRepo = "https://github.com/dehbbyk/dok_app";
+  static const String hngHirePage = "https://hng.tech/hire";
+  static const String telex = "https://telex.ai";
+  static const String delve = "https://delve.hng.tech";
+}
